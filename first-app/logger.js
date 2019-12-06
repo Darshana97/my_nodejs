9d2f6dc43a8298ac22c1f4863bc181489d2f377c
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-12-06 08:02:43 +0530

    Create logger.js

---
+++ first-app/logger.js
@@ -0,0 +1,19 @@
+
+
+console.log(__filename);
+console.log(__dirname);
+
+var url = 'http://mylogger.io/log';
+
+function log(message) {
+    //send an http request
+    console.log(message);
+}
+
+module.exports = log;
+
+module.exports.log = log;
+exports.log = log;
+
+exports = log;  //module.exports
+
